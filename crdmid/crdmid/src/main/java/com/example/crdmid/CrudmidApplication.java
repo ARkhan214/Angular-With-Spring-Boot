3a074797f1935b462f7784843b32707d8ffce1b7
ARkhan214 <abdurrahimkhan214dc@gmail.com>
2025-07-31 13:09:52 +0600

    add

---
+++ crdmid/crdmid/src/main/java/com/example/crdmid/CrudmidApplication.java
@@ -0,0 +1,13 @@
+package com.example.crdmid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudmidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudmidApplication.class, args);
+	}
+
+}
---
+++ crdmid/crdmid/src/test/java/com/example/crdmid/CrudmidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crdmid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudmidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
