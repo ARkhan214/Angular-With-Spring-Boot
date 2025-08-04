ce343af78e8fbda111c9448ee462d81c5a1eaf47
MD RAHIM KHAN <abdurrahimkhan214dc@gmail.com>
2025-08-04 18:58:30 +0600

    add

--- Spring+Angular/progect/src/main/java/com/emranhss/progect/jwt/JwtService.java
+++ Spring+Angular/progect/src/main/java/com/emranhss/progect/jwt/JwtService.java
@@ -1,15 +1,22 @@
 package com.emranhss.progect.jwt;
 
 
+import com.emranhss.progect.entity.Token;
+import com.emranhss.progect.entity.User;
 import com.emranhss.progect.repository.ITokenRepository;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
+import javax.xml.crypto.Data;
 import java.util.Base64;
+import java.util.Date;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
@@ -32,8 +39,56 @@ public class JwtService {
     }
 
     private SecretKey getSigningKey() {
-        byte[] kebByte = Base64.Decoder.BASE64URL.decode(SECREAT_KEY);
+        byte[] kebByte = Decoders.BASE64URL.decode(SECREAT_KEY);
         return Keys.hmacShaKeyFor(kebByte);
+    }
+
+
+    public String generateToken(User  user ){
+        return  Jwts.builder()
+                .setSubject(user.getEmail())
+                .claim("role",user.getRole())
+                .setIssuedAt(new Date(System.currentTimeMillis()+24*60*60*1000))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    public String extractUsername(String token){
+
+        return extractClaim(token,Claims::getSubject);
+
+    }
+
+
+    // Extract a specific Claim from the Token Claims
+    public <T> T extractClaim(String token, Function<Claims,T>resolve){
+        Claims claims = extractAllClaims(token);
+        return resolve.apply(claims);
+    }
+
+private Date extractExpiration(String token){
+        return extractClaim(token,Claims::getExpiration);
+}
+
+private boolean isTokenExpired(String token){
+        return extractExpiration(token).before(new Date());
+}
+
+public boolean isValid(String token, UserDetails user){
+
+        String username = extractUsername(token);
+
+        boolean validToken = tokenRepository
+                .findByToken(token)
+                .map(t-> !t.isLogout())
+                .orElse(false);
+        return (username.equals(user.getUsername()) && !isTokenExpired(token) && validToken);
+}
+
+
+//get user role from token
+    public String extractUserRole(String token){
+        return  extractClaim(token,claims -> claims.get("role",String.class));
     }
 
 
---
+++ Spring+Angular/progect/src/main/java/com/emranhss/progect/security/SecurityConfig.java
@@ -0,0 +1,87 @@
+package com.emranhss.progect.security;
+
+
+import com.emranhss.progect.jwt.JwtAuthenticationFilter;
+import com.emranhss.progect.jwt.JwtService;
+import com.emranhss.progect.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+    @Autowired
+    private UserService userService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http,
+                                           JwtAuthenticationFilter jwtAuthenticationFilter,
+                                           UserService userService) throws Exception {
+
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests(req ->
+                        req.requestMatchers("/api/user/", "/images/**", "/api/jobseeker/**", "/api/user/active/**", "/api/user/login")
+                                .permitAll()
+                                .requestMatchers("/api/user/all")
+                                .hasAuthority("JOBSEEKER")
+                                .anyRequest().authenticated()
+                ).userDetailsService(userService)
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter(JwtService jwtService, UserService userService) throws Exception {
+        return new JwtAuthenticationFilter(jwtService, userService);
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:4200", "http://127.0.0.1:4200"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+
+    }
+
+}
