ea702eda34418fc28a7697e6dde925bcd568a694
ARkhan214 <abdurrahimkhan214dc@gmail.com>
2025-08-06 13:06:32 +0600

    add

--- Spring+Angular/progect/src/main/java/com/emranhss/progect/repository/ITokenRepository.java
+++ Spring+Angular/progect/src/main/java/com/emranhss/progect/repository/ITokenRepository.java
@@ -9,15 +9,27 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface ITokenRepository extends JpaRepository<Token,Long> {
+public interface ITokenRepository extends JpaRepository<Token, Long> {
+
     Optional<Token> findByToken(String token);
 
-
     @Query("""
-            Select t from Token t inner join User u on t.user.id = u.id
-            where t.user.id = :userId and t.logout = false
+            Select t from Token t inner join User u on t.user.id=u.id
+            where t.user.id= :userId and t.logout=false 
             """)
     List<Token> findAllTokenByUser(int userId);
+
+
+
+
+
+//    Optional<Token> findByToken(String token);
+//
+//    @Query("""
+//        SELECT t FROM Token t
+//        WHERE t.user.id = :userId AND t.logout = false
+//    """)
+//    List<Token> findAllValidTokensByUserId(int userId); // ✅ meaningful name
 
 
 
